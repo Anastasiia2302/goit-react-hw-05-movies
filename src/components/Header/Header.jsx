8161f3262f50b006044bc2c9457271223adfd8ab
Anastasiia Herusova <nastia.herusova@gmail.com>
2023-04-03 13:35:29 +0300

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,23 @@
-
-import { HeaderLink, HeaderList, HeaderText } from "./Header.styled";
+import { Outlet } from 'react-router-dom';
+import { HeaderLink, HeaderList, HeaderText } from './Header.styled';
 
 const navItems = [
   { href: '/', text: 'Home' },
   { href: 'movies', text: 'Movies' },
 ];
- const Header = () => {
+const Header = () => {
   return (
-    <HeaderList>
-      {navItems.map(({ href, text }) => (
-        <HeaderText key={href}>
-          <HeaderLink to={href}>{text}</HeaderLink>
-        </HeaderText>
-      ))}
-    </HeaderList>
+    <>
+      <HeaderList>
+        {navItems.map(({ href, text }) => (
+          <HeaderText key={href}>
+            <HeaderLink to={href}>{text}</HeaderLink>
+          </HeaderText>
+        ))}
+      </HeaderList>
+
+      <Outlet />
+    </>
   );
 };
 export default Header;
